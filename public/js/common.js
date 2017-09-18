865d6d3ac1d0e36e798350725b2223b3ec0f15da
Moment-of-Romance <540232076@qq.com>
2017-09-18 16:22:46 +0800

    完善登录功能并实现了退出登录

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,40 @@
+NProgress.start();
 
-	NProgress.start();
+NProgress.done();
 
-	NProgress.done();
+$('.navs ul').prev('a').on('click', function () {
+    $(this).next().slideToggle();
+});
 
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+//实现退出登录
+$("#exists").on("click", function () {
+    $.ajax({
+        type : 'post',
+        url : '/api/logout',
+        dataType : 'json',
+        success : function (data) {
+            if(data.code == 200){
+                location.href = 'main/login';
+            }
+        }
+    })
+});
+
+
+//如果不是首次登陆则直接跳转到登陆页面
+var loginInfo = $.cookie('loginInfo');
+//将字符串 转变成json对象
+loginInfo = loginInfo && JSON.parse(loginInfo);
+
+var PHPSESSID = $.cookie('PHPSESSID');
+//登陆后将 用户信息 渲染到页面上
+//修改头像
+$('.profile .img-circle img').attr('src',loginInfo.tc_avatar);
+//修改用户名
+$('.profile h4').html(loginInfo.tc_name);
+
+
+//如果没有登录 则跳转到 登录页面
+if(!PHPSESSID){
+    location.href = '/main/login';
+}
